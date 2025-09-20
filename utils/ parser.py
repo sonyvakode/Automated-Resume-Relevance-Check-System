120744675c36b4da8b5ee798dbb9805465b2b8f2
sonyvakode <sonyvakode648@gmail.com>
2025-09-20 19:54:00 +0530

    Update  parser.py

--- utils/ parser.py
+++ utils/ parser.py
@@ -1,20 +1,15 @@
 import fitz  # PyMuPDF
 import docx2txt
-from io import BytesIO
 
 def extract_text(file):
-    """Extract text from PDF or DOCX file"""
     filename = file.name.lower()
     if filename.endswith(".pdf"):
-        pdf_text = ""
-        pdf_bytes = BytesIO(file.read())
-        doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+        doc = fitz.open(stream=file.read(), filetype="pdf")
+        text = ""
         for page in doc:
-            pdf_text += page.get_text()
-        return pdf_text
-    elif filename.endswith(".docx"):
-        docx_bytes = BytesIO(file.read())
-        text = docx2txt.process(docx_bytes)
+            text += page.get_text()
         return text
+    elif filename.endswith(".docx"):
+        return docx2txt.process(file)
     else:
-        return file.read().decode("utf-8", errors="ignore")
+        return file.read().decode("utf-8")
